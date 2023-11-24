4782b64ef3e5df77dcee821d7ff85d133e3705d3
Dan McGrath <mcgrathd3254@yahoo.com>
2023-11-24 17:50:44 -0500

    add post for comments

--- src/routes/comment.js
+++ src/routes/comment.js
@@ -3,14 +3,17 @@ import {
   comment_of_comment_detail_get,
   comments_of_comments_get,
   post_comment_detail_get,
+  post_comment_post,
   post_comments_get,
 } from "../controllers/commentController";
 
 const router = Router();
 
 router.get("/:postId/comments", post_comments_get);
+router.post("/:postId/comments", post_comment_post);
 router.get("/:postId/comments/:commentId", post_comment_detail_get);
 router.get("/:postId/comments/:commentId/comments", comments_of_comments_get);
+router.post("/:postId/comments/:commentId", post_comment_post);
 router.get(
   "/:postId/comments/:commentId/comments/:cocId",
   comment_of_comment_detail_get
