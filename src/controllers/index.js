8674b18ce3d3beba23b620220855b9412958669d
Dan McGrath <mcgrathd3254@yahoo.com>
2023-11-23 22:40:30 -0500

    create index file for controllers

---
+++ src/controllers/index.js
@@ -0,0 +1,9 @@
+import commentController from "./commentController";
+import postController from "./postController";
+import userController from "./userController";
+
+export default {
+  commentController,
+  postController,
+  userController,
+};
