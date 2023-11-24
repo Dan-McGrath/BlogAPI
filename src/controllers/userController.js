63e0e33eb3360b3f1eb3e189e956fe1590381a80
Dan McGrath <mcgrathd3254@yahoo.com>
2023-11-24 03:05:56 -0500

    remove for testing

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -3,9 +3,6 @@ import User from "../models/user";
 import asyncHandler from "express-async-handler";
 
 export const users_get = asyncHandler(async (req, res, next) => {
-  if (req.user === undefined) {
-    res.json("/login");
-  }
   const users = await User.find().exec();
   res.json({
     users: users,
