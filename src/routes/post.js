757df6ef13d5e0ef8f7c19e70ffe05ff381aa053
Dan McGrath <mcgrathd3254@yahoo.com>
2023-11-24 17:02:14 -0500

    add get routes with controllers

--- src/routes/post.js
+++ src/routes/post.js
@@ -1,5 +1,9 @@
 import { Router } from "express";
+import { posts_get, posts_detail_get } from "../controllers/postController";
 
 const router = Router();
+
+router.get("/", posts_get);
+router.get("/:postId", posts_detail_get);
 
 export default router;
