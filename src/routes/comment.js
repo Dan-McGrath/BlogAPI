04232c999148f21245c2bf30dc628449e3ccceed
Dan McGrath <mcgrathd3254@yahoo.com>
2023-11-24 17:02:04 -0500

    make get routes with controllers

--- src/routes/comment.js
+++ src/routes/comment.js
@@ -1,5 +1,19 @@
 import { Router } from "express";
+import {
+  comment_of_comment_detail_get,
+  comments_of_comments_get,
+  post_comment_detail_get,
+  post_comments_get,
+} from "../controllers/commentController";
 
 const router = Router();
+
+router.get("/:postId/comments", post_comments_get);
+router.get("/:postId/comments/:commentId", post_comment_detail_get);
+router.get("/:postId/comments/:commentId/comments", comments_of_comments_get);
+router.get(
+  "/:postId/comments/:commentId/comments/:cocId",
+  comment_of_comment_detail_get
+);
 
 export default router;
