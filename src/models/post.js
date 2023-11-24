1f1b3f09c99021a875d7df467c3b63ce81f6bbfe
Dan McGrath <mcgrathd3254@yahoo.com>
2023-11-23 21:53:09 -0500

    update schema for posts

--- src/models/post.js
+++ src/models/post.js
@@ -2,15 +2,23 @@ import mongoose from "mongoose";
 
 const PostSchema = new mongoose.Schema(
   {
-    text: {
+    title: {
       type: String,
       required: true,
+      maxLength: 100,
+    },
+    isPublished: {
+        type: Boolean,
+        default: false,
+      },
+    article: {
+        type: String,
+        required: true,
     },
     user: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
   },
   { timestamps: true }
 );
-
 
 const Post = mongoose.model("Post", PostSchema);
 
