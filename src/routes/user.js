cccb2b55b7346dcc8e9f2bb4471669abe66c3780
Dan McGrath <mcgrathd3254@yahoo.com>
2023-11-24 17:02:32 -0500

    add detail get route with controller

--- src/routes/user.js
+++ src/routes/user.js
@@ -1,8 +1,8 @@
 import { Router } from "express";
-import { users_get } from "../controllers/userController";
+import { user_detail_get, users_get } from "../controllers/userController";
 
 const router = Router();
 
 router.get("/", users_get);
-
+router.get("/:userId", user_detail_get);
 export default router;
