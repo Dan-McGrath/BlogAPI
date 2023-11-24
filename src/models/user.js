ac8ef789ea58c413bd4f56bbfffa6d8c3f4804fd
Dan McGrath <mcgrathd3254@yahoo.com>
2023-11-24 03:06:10 -0500

    made virtuals show up for json

--- src/models/user.js
+++ src/models/user.js
@@ -12,12 +12,14 @@ const UserSchema = new mongoose.Schema(
     },
     password: { type: String, required: true },
   },
-  { timestamps: true }
+  { timestamps: true, toJSON: { virtuals: true } }
 );
 
-UserSchema.virtual("full_name").get(function () {
+UserSchema.virtual("full_name").get(
+  function () {
     return `${this.first_name} ${this.last_name}`;
-  });
+  },
+);
 
 UserSchema.statics.findByLogin = async function (login) {
   let user = await this.findone({
