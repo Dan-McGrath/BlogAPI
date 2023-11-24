09ee4b7b136dfded9f91cff7e16567baf2aa1792
Dan McGrath <mcgrathd3254@yahoo.com>
2023-11-24 17:01:37 -0500

    add get controllers

---
+++ src/controllers/postController.js
@@ -0,0 +1,17 @@
+import Post from "../models/post";
+
+import asyncHandler from "express-async-handler";
+
+export const posts_get = asyncHandler(async (req, res, next) => {
+  const posts = await Post.find().exec();
+  res.json({
+    posts: posts,
+  });
+});
+
+export const posts_detail_get = asyncHandler(async (req, res, next) => {
+  const post = await Post.findById(req.params.postId).exec();
+  res.json({
+    post: post,
+  });
+});
