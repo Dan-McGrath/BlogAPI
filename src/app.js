3882c108f726d130292852f6a2ee7d1aec769353
Dan McGrath <mcgrathd3254@yahoo.com>
2023-11-23 21:29:01 -0500

    add error handler and passport functions

--- src/app.js
+++ src/app.js
@@ -1,17 +1,94 @@
 import "dotenv/config";
 import cors from "cors";
 import express from "express";
+import mongoose from "mongoose";
+import session from "express-session";
+import passport from "passport";
+import LocalStrategy from "passport-local";
+import bycrypt from "bcryptjs";
 import models, { connectDb } from "./models";
 
 const app = express();
 
+// MongooseDB setup
+mongoose.set("strictQuery", false);
+const mongoDB = process.env.DB_URL;
+
+main().catch((err) => console.log(err));
+async function main() {
+  return await mongoose.connect(mongoDB);
+}
+
+// Passport setup
+passport.use(
+  new LocalStrategy(async (username, password, done) => {
+    try {
+      const user = await models.User.findOne({ username: username });
+      if (!user) {
+        return done(null, false, { message: "Incorrect username" });
+      }
+      const match = await bycrypt.compare(password, user.password);
+      if (!match) {
+        return done(null, false, { message: "Incorrect password" });
+      }
+      return done(null, user);
+    } catch (err) {
+      return done(err);
+    }
+  })
+);
+
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+passport.deserializeUser(async (id, done) => {
+  try {
+    const user = await models.User.findById(id);
+    done(null, user);
+  } catch (err) {
+    done(err);
+  }
+});
+
+app.use(
+  session({
+    secret: process.env.SECRET_KEY,
+    resave: false,
+    saveUninitialized: true,
+  })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-connectDb().then(async () => {
-    app.listen(process.env.PORT, () =>
-      console.log(`Listening on port ${process.env.PORT}`)
-    );
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function (err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get("env") === "development" ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+
+  res.json({
+    message: err.message,
+    error: err,
   });
-  
+});
+
+app.listen(process.env.PORT, () =>
+  console.log(`Listening on port ${process.env.PORT}`)
+);
