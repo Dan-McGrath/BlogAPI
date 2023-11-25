d2d4f00deda0948da25e8aa6c78a10027d6fcf42
Dan McGrath <mcgrathd3254@yahoo.com>
2023-11-24 21:27:44 -0500

    add create post route

--- src/routes/post.js
+++ src/routes/post.js
@@ -1,9 +1,14 @@
 import { Router } from "express";
-import { posts_get, posts_detail_get } from "../controllers/postController";
+import {
+  posts_get,
+  posts_detail_get,
+  post_create_post,
+} from "../controllers/postController";
 
 const router = Router();
 
 router.get("/", posts_get);
+router.post("/create", post_create_post);
 router.get("/:postId", posts_detail_get);
 
 export default router;
