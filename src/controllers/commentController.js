ee4a7b584322b6c257ffcee18f849f4d4c94df96
Dan McGrath <mcgrathd3254@yahoo.com>
2023-11-24 22:14:18 -0500

    comment out potentially unneeded controllers

--- src/controllers/commentController.js
+++ src/controllers/commentController.js
@@ -2,37 +2,7 @@ import Comment from "../models/comment";
 import { body, validationResult } from "express-validator";
 import asyncHandler from "express-async-handler";
 
-export const post_comments_get = asyncHandler(async (req, res, next) => {
-  const comments = await Comment.find({ post: req.params.postId }).exec();
-  res.json({
-    comments: comments,
-  });
-});
 
-export const post_comment_detail_get = asyncHandler(async (req, res, next) => {
-  const comment = await Comment.findById(req.params.commentId).exec();
-  res.json({
-    comment: comment,
-  });
-});
-
-export const comments_of_comments_get = asyncHandler(async (req, res, next) => {
-  const comments = await Comment.find({
-    parentComment: req.params.commentId,
-  }).exec();
-  res.json({
-    comments: comments,
-  });
-});
-
-export const comment_of_comment_detail_get = asyncHandler(
-  async (req, res, next) => {
-    const comment = await Comment.findById(req.params.cocId).exec();
-    res.json({
-      comment: comment,
-    });
-  }
-);
 
 export const post_comment_post = [
   body("name").trim().escape(),
@@ -64,3 +34,37 @@ export const post_comment_post = [
     }
   }),
 ];
+
+// export const post_comments_get = asyncHandler(async (req, res, next) => {
+//   const comments = await Comment.find({ post: req.params.postId }).exec();
+//   res.json({
+//     comments: comments,
+//   });
+// });
+
+// export const post_comment_detail_get = asyncHandler(async (req, res, next) => {
+//   const comment = await Comment.findById(req.params.commentId).exec();
+//   res.json({
+//     comment: comment,
+//   });
+// });
+
+// export const comments_of_comments_get = asyncHandler(async (req, res, next) => {
+//   const comments = await Comment.find({
+//     parentComment: req.params.commentId,
+//   }).exec();
+//   res.json({
+//     comments: comments,
+//   });
+// });
+
+// export const comment_of_comment_detail_get = asyncHandler(
+//   async (req, res, next) => {
+//     const comment = await Comment.findById(req.params.cocId).exec();
+//     res.json({
+//       comment: comment,
+//     });
+//   }
+// );
+
+
