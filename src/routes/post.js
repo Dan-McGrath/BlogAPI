da6babb1c940304bb65ed63a05efa5c01fd80ee3
Dan McGrath <mcgrathd3254@yahoo.com>
2023-11-24 22:15:45 -0500

    add routes for delete and update and post for comments

--- src/routes/post.js
+++ src/routes/post.js
@@ -4,11 +4,17 @@ import {
   posts_detail_get,
   post_create_post,
 } from "../controllers/postController";
+import { post_comment_post } from "../controllers/commentController";
 
 const router = Router();
 
 router.get("/", posts_get);
 router.post("/create", post_create_post);
 router.get("/:postId", posts_detail_get);
+router.post("/:postId", post_comment_post);
+router.delete("/:postId");
+
+router.put("/:postId/update");
+
 
 export default router;
