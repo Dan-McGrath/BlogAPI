574a36a094bc013b51e1c79fa535c4394ded213d
Dan McGrath <mcgrathd3254@yahoo.com>
2023-11-24 22:16:02 -0500

    add routes for update and delete

--- src/routes/user.js
+++ src/routes/user.js
@@ -5,4 +5,7 @@ const router = Router();
 
 router.get("/", users_get);
 router.get("/:userId", user_detail_get);
+router.put("/:userId/update");
+router.delete("/:userId");
+
 export default router;
