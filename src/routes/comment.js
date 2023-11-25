96090819f08ffb24c757335533da87d8901be0c7
Dan McGrath <mcgrathd3254@yahoo.com>
2023-11-24 22:15:09 -0500

    comment out potentially unneded routes

--- src/routes/comment.js
+++ src/routes/comment.js
@@ -9,14 +9,18 @@ import {
 
 const router = Router();
 
-router.get("/:postId/comments", post_comments_get);
-router.post("/:postId/comments", post_comment_post);
-router.get("/:postId/comments/:commentId", post_comment_detail_get);
-router.post("/:postId/comments/:commentId", post_comment_post);
-router.get("/:postId/comments/:commentId/comments", comments_of_comments_get);
-router.get(
-  "/:postId/comments/:commentId/comments/:cocId",
-  comment_of_comment_detail_get
-);
+router.delete('/:postId')
+
+// router.get("/:postId/comments", post_comments_get);
+// router.post("/:postId/comments", post_comment_post);
+// router.get("/:postId/comments/:commentId", post_comment_detail_get);
+// router.post("/:postId/comments/:commentId", post_comment_post);
+// router.put("/:postId/comments/:commentId");
+// router.delete("/postId/comments/:commentId");
+// router.get("/:postId/comments/:commentId/comments", comments_of_comments_get);
+// router.get(
+//   "/:postId/comments/:commentId/comments/:cocId",
+//   comment_of_comment_detail_get
+// );
 
 export default router;
