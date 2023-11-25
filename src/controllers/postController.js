10b89e5ae33523f1bff2084eb55d02fc25355354
Dan McGrath <mcgrathd3254@yahoo.com>
2023-11-24 21:27:35 -0500

    create post controller

--- src/controllers/postController.js
+++ src/controllers/postController.js
@@ -16,3 +16,30 @@ export const posts_detail_get = asyncHandler(async (req, res, next) => {
     post: post,
   });
 });
+
+export const post_create_post = [
+  body("title").trim().isLength({ min: 1, max: 100 }).escape(),
+  body("article").trim().escape(),
+
+  asyncHandler(async (req, res, next) => {
+    const errors = validationResult(req);
+    const post = new Post({
+      title: req.body.title,
+      article: req.body.article,
+      user: req.user,
+    });
+
+    if (!errors.isEmpty()) {
+      res.json({
+        post: post,
+        errors: errors.array(),
+      });
+      return;
+    } else {
+      await post.save();
+      res.json({
+        post: post,
+      });
+    }
+  }),
+];
